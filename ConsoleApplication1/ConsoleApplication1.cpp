49226899b5c6c0accccaf20f367ebc36fd4f0c00
4D-09\koyo <a>
2026-07-07 14:08:03 +0900

    コメントの追加

--- ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -3,12 +3,25 @@
 class GameManager
 {
 private:
+	/// <summary>
+	/// オブジェクトを外部から生成するのを防ぐためのprivateコンストラクタ
+	/// </summary>
 	GameManager() {}
 
 public:
+	/// <summary>
+	/// 別のオブジェクトのコピーを禁止するためのコピーコンストラクタ
+	/// </summary>
 	GameManager(const GameManager&) = delete;
+	/// <summary>
+	/// 別のオブジェクトのコピーを禁止するためのコピーコンストラクタ
+	/// </summary>
 	GameManager& operator=(const GameManager&) = delete;
 
+	/// <summary>
+	/// スレッドセーフな、GameManagerの唯一のオブジェクトを返すことができる
+	/// </summary>
+	/// <returns></returns>
 	static GameManager& Instance()
 	{
 		static GameManager instance;
@@ -24,4 +37,5 @@ public:
 int main()
 {
 	GameManager::Instance().StartGame();
+
 }
