704822729e2323a09f28ed25104d8090310d6c4b
4D-09\koyo <a>
2026-07-07 13:39:05 +0900

    プロジェクト ファイルを追加します。

---
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+
+class GameManager
+{
+private:
+	GameManager() {}
+
+public:
+	GameManager(const GameManager&) = delete;
+	GameManager& operator=(const GameManager&) = delete;
+
+	static GameManager& Instance()
+	{
+		static GameManager instance;
+		return instance;
+	}
+
+	void StartGame()
+	{
+		std::cout << "Hello World!\n";
+	}
+};
+
+int main()
+{
+	GameManager::Instance().StartGame();
+}
